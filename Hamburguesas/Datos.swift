b745c91096a14dc993b4a7cf02688a5cf8c93304
Jose Luis Garcia Dueñas <joseluisgarciad@gmail.com>
2016-03-26 15:18:01 +0100

    Inicial

---
+++ Hamburguesas/Datos.swift
@@ -0,0 +1,9 @@
+//
+//  Datos.swift
+//  Hamburguesas
+//
+//  Created by Jose Luis Garcia Dueñas on 26/3/16.
+//  Copyright © 2016 Jose Luis Garcia Dueñas. All rights reserved.
+//
+
+import Foundation
